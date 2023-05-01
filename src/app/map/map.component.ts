0cce02adb3dc308f1ea345296f9e91415de8e5d4
medaminegannouni <92208611+medaminegannouni@users.noreply.github.com>
2023-05-01 14:59:11 +0200

    calcul direction

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component} from '@angular/core';
 import  * as L from 'leaflet';
 import {Observable, Subscriber} from "rxjs";
 import {MapboxService} from "../mapbox.service";
+import {marker} from "leaflet";
 
 
 @Component({
@@ -13,14 +14,14 @@ export class MapComponent  implements  AfterViewInit {
   latitude!: number;
   longitude!: number;
   map: any;
-constructor(private service:MapboxService) {
-}
+  constructor(private service:MapboxService) {
+  }
 
 
   ngAfterViewInit(): void {
     this.loadMap();
 
-    }
+  }
 
 
   private getCurrentPosition(): any {
@@ -44,6 +45,8 @@ constructor(private service:MapboxService) {
     return this.service.getInfo(y1.toString(), x1.toString(), y2.toString(), x2.toString());
   }
   private loadMap(): void {
+    let  currentLat:number;
+    let  currentLng:number;
     this.map = L.map('map').setView([0, 0], 1);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -53,6 +56,22 @@ constructor(private service:MapboxService) {
       zoomOffset: -1,
       accessToken: 'sk.eyJ1IjoibWVkYW1pbmU0NyIsImEiOiJjbGd6MDFpZngwZW5pM2pwbnJybGkxM2JiIn0.AAF5Kb1dlDUNY3-Rp6--1g',
     }).addTo(this.map);
+
+    this.getCurrentPosition().subscribe((position: any) => {
+        currentLat = position.latitude;
+        currentLng = position.longitude;
+
+      this.map.flyTo([currentLat, currentLng], 18);
+      const icon = L.icon({
+        iconUrl: 'assets/images/marker-icon.png',
+        shadowUrl: 'assets/images/marker-shadow.png',
+        popupAnchor: [13, 0],
+      });
+
+      const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Here I Am');
+      marker.addTo(this.map);
+    });
+
 
     this.map.on('click', (event: L.LeafletMouseEvent) => {
       const latitude = event.latlng.lat;
@@ -65,7 +84,11 @@ constructor(private service:MapboxService) {
       });
 
       const marker = L.marker([latitude, longitude], { icon }).addTo(this.map)
+      this.getInfo( currentLat, currentLng, latitude, longitude)
+        .then((result: any) => {
+          alert(`Distance: ${result.routes[0].distance.toFixed(2)} meters\nDuration: ${result.routes[0].duration.toFixed(2)} seconds`);
 
+        });
       // remove the marker when the user clicks on it
       marker.on('click', () => {
         this.map.removeLayer(marker);
@@ -74,24 +97,7 @@ constructor(private service:MapboxService) {
 
 
 
-    this.getCurrentPosition()
-      .subscribe((position: any) => {
-        this.map.flyTo([position.latitude, position.longitude], 18);
 
-        const icon = L.icon({
-          iconUrl: 'assets/images/marker-icon.png',
-          shadowUrl: 'assets/images/marker-shadow.png',
-          popupAnchor: [13, 0],
-        });
-
-        const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Angular Leaflet');
-        marker.addTo(this.map);
-      });
-    this.getInfo(52.52044, 13.404777, 52.520008, 13.404954)
-    .then((result: any) => {
-        alert(`Distance: ${result.routes[0].distance.toFixed(2)} meters\nDuration: ${result.routes[0].duration.toFixed(2)} seconds`);
-
-    });
 
 
   }
